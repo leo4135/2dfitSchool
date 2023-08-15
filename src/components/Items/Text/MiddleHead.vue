cb2da072d9ebabf5d5b5111167d66876c683f3a1
User <leo4135@yandex.ru>
2023-08-15 17:55:20 +0300

    Update Cards

---
+++ src/components/Items/Text/MiddleHead.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+defineProps({
+  text: Object,
+})
+
+</script>
+
+<template>
+
+  <h2 class="banner__main__h2"> {{ text }} </h2>
+
+</template>
+
+
+<style scoped>
+.banner__main__h2 {
+  font-style: italic;
+  font-weight: bold;
+  font-size: 34px;
+  display: flex;
+  flex-direction: column;
+  line-height: 0.8;
+}
+</style>
